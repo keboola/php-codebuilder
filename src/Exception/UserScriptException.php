5ddbefe2cbbb2a053c2eb1fa0c34dbd05de82ca5
Výborný Adam <mail@adamvyborny.cz>
2022-03-28 20:18:29 +0200

    PHPCS fixes

--- src/Exception/UserScriptException.php
+++ src/Exception/UserScriptException.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Keboola\Code\Exception;
 
-class UserScriptException extends \Exception
+use Exception;
+
+class UserScriptException extends Exception
 {
     public function __construct(string $message)
     {
